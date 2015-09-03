626b99afd9727999afc8d7cda8c4fd571d237e95
phylp <philson3@gmail.com>
2015-09-03 14:43:42 -0700

    header parser module and index.js  created

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 'use strict';
+var fs = require('fs');
 
-var headerParser = require(__dirname + "/lib/header_parser");
+var headerParser = require(__dirname + "/lib/header_parser.js");
+
+var header = {};
+
+fs.readFile(__dirname + '/img/non-palette-bitmap.bmp', callHP); 
+
+function callHP(err, data){
+  if(err){
+	throw err;
+  }
+  headerParser(data, header);
+};
+
+
+
