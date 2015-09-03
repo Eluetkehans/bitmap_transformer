392a9cd4d96ecba1cc34041586ca15ad3b0c7d4a
Erik Luetkehans <eluetekhans@gmail.com>
2015-09-02 21:40:08 -0700

    prep transformer_test.js

--- test/transformer_test.js
+++ test/transformer_test.js
@@ -1,1 +1,16 @@
 'use strict';
+
+var expect = require('chai').expect;
+var index = require(__dirname + '/../index');
+
+describe('index', function() {
+  it('should call all necessary modules');
+});
+
+/*
+TODO bit map file header parser.
+  -should read the first two bytes and return type. (BM type manditory)
+  -should read the image size. offset 2, size 4 bytes.
+    (we will probably have to use this to find the pallet)
+*/
+
